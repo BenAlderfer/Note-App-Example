b1f0d600be9cf2ec7823ac9ae734eea48d067430
Ben Alderfer <benalderfer974@gmail.com>
2017-04-26 22:49:54 -0400

    added saving/restoring list

--- app/src/main/java/com/alderferstudios/noteappexample/MainActivity.java
+++ app/src/main/java/com/alderferstudios/noteappexample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.alderferstudios.noteappexample;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,10 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     //the list of items
     private List<String> listItems;
     private ArrayAdapter<String> adapter;
+
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
 
     /**
      * Executes on app start
@@ -31,7 +40,18 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        listItems = new ArrayList<>();
+        prefs = getPreferences(Context.MODE_PRIVATE);
+        editor = prefs.edit();
+
+        //read in saved list
+        String savedList = prefs.getString("list", "");
+        if (savedList.equals("")) { //nothing saved
+            listItems = new ArrayList<>();
+        } else {    //restore saved
+            Type type = new TypeToken<ArrayList<String>>(){}.getType();
+            listItems = new Gson().fromJson(savedList, type);
+        }
+
         ListView list = (ListView) findViewById(R.id.list);
 
         // adapter to tie listItems to list
@@ -40,6 +60,21 @@ public class MainActivity extends AppCompatActivity {
 
         // assign the list adapter
         list.setAdapter(adapter);
+    }
+
+    /**
+     * Save list on app close
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        //turn list into json
+        String items = new Gson().toJson(listItems);
+
+        //save list to cache
+        editor.putString("list", items);
+        editor.apply(); //use apply, its faster than commit
     }
 
     /**
